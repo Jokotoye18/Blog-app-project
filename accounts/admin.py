b4080764613f037d68cc192523d44651ce3ed792
jokotoye18 <jokotoyeademola95@gmail.com>
2020-03-22 18:05:12 +0100

    login,logout and base template update with signup(.)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['email', 'username', 'is_active', 'is_staff', ]
+
+admin.site.register(User, CustomUserAdmin)
