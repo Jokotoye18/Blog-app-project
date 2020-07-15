a37006c4c4e83f36e1beb76be1bb176806b9f5bb
jokotoye18 <jokotoyeademola95@gmail.com>
2020-07-15 11:53:55 +0100

    dj_database_url

--- posts/settings.py
+++ posts/settings.py
@@ -138,6 +138,10 @@ DATABASES = {
     },
 }
 
+DATABASE_URL = config('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
